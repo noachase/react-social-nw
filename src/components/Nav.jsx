ea1cc54a40515cbd1e22cc91a69613c80986480c
Panacea <noach4se@gmail.com>
2020-03-11 22:15:59 +0200

    added .active classe to css module nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import classes from './Nav.module.css';
 const Nav = () => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}>Profile</div>
+            <div className={`${classes.item} ${classes.active}`}>Profile</div>
             <div className={classes.item}>Messages</div>
             <div className={classes.item}>News</div>
             <div className={classes.item}>Music</div>
